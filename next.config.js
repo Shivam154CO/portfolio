c939d196e83f77837328e4acb46f42856efa6b9d
Shivam Pawar | Contributer <110741650+Shivam154CO@users.noreply.github.com>
2025-11-03 19:58:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'zpcjcjqhhswcyaygtmxh.supabase.co',
+      'cdn-icons-png.flaticon.com'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'zpcjcjqhhswcyaygtmxh.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/assets/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
